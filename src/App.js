79831aeed8f867e4c5289e6abb09c22886fc8d7b
alexisglez78 <alexisglez78@gmail.com>
2021-02-09 00:51:02 -0600

    v

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import Tabla from "./ui/components/Tabla";
 const fetchOffersList = () => {
     return [];
 };
-const estiloImg = {
-
-}
+const estiloImg = {};
 export default function App() {
     return (
         <div>
-        <img style={{
- opacity: 0.2,
- position:'absolute',
- height:'100%',
- width:'100%'
-    }} src="https://kinsta.com/es/wp-content/uploads/sites/8/2020/01/prevencion-del-fraude-en-el-comercio-electronico.png" />
-        <Cabecera></Cabecera>
-        <Tabla></Tabla>
+            <img
+                style={{
+                    opacity: 0.2,
+                    position: "absolute",
+                    height: "100%",
+                    width: "100%",
+                }}
+                src="https://kinsta.com/es/wp-content/uploads/sites/8/2020/01/prevencion-del-fraude-en-el-comercio-electronico.png"
+            />
+            <Cabecera />
+            <Tabla />
         </div>
     );
 }
